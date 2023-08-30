6c48fcd182d84eddd66104c9e68a696f8d519c93
Bret Barden <bret.barden@gmail.com>
2023-08-30 01:46:03 -0400

    lab complete

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
-def add(num1, num2):
-    pass
+def add(num1 = 1, num2 = 2):
+    return num1 + num2
 
-def halve(number):
-    pass
+def halve(number = 100):
+    return number / 2
